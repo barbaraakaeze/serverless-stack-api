55a225646b7a8509a904bd3a6a02803f1cb2659e
[BarbaraAkaeze] <[barbaraakaeze@gmail.com]>
2021-02-11 10:07:26 -0500

    Adding my serverless API

---
+++ notes-api/delete.js
@@ -0,0 +1,17 @@
+// Delete a note given an ID
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: "123",
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  await dynamoDb.delete(params);
+
+  return { status: true };
+});
